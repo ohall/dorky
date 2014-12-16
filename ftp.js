21e67f0d6ec538391de6caa674916583ca63084a
ohall <ohall@grassroots-tech.com>
2014-12-16 16:58:05 -0500

    wrap in funcs for namespacing

--- ftp.js
+++ ftp.js
@@ -2,27 +2,30 @@
  * Created by Oakley Hall on 12/15/14.
  */
 
-
-module.exports = function (host, user, password, callback) {
+(function () {
     'use strict';
-    var Client = require('ftp'),
-        fs = require('fs'),
-        client = new Client();
 
-    client.on('ready', function () {
-        client.put('foo.txt', 'foo.remote-copy.txt', function (err) {
-            if (err) {
-                throw err;
-            }
-            client.end();
+    module.exports = function (host, user, password, callback) {
+
+        var Client = require('ftp'),
+            fs = require('fs'),
+            client = new Client();
+
+        client.on('ready', function () {
+            client.put('foo.txt', 'foo.remote-copy.txt', function (err) {
+                if (err) {
+                    throw err;
+                }
+                client.end();
+            });
         });
-    });
 
-    client.connect({
-        host: host,
-        user: user,
-        password: password
-    });
+        client.connect({
+            host: host,
+            user: user,
+            password: password
+        });
 
-    callback();
-};
+        callback();
+    };
+})();
--- podsmash.js
+++ podsmash.js
@@ -2,15 +2,18 @@
  * Created by Oakley Hall on 12/15/14.
  */
 
+(function () {
+    'use strict';
 
-console.log( 'PodSmash!' );
-var ftp = require('./ftp');
+    console.log( 'PodSmash!' );
+    var ftp = require('./ftp');
 
-if( process.argv[2] && process.argv[3] && process.argv[4] ){
-    ftp( process.argv[2], process.argv[3], process.argv[4], function () {
-        console.log('PodSmash complete!');
-    });
-}else{
-    console.log('Requires args: FTP Host, User, Password');
+    if( process.argv[2] && process.argv[3] && process.argv[4] ){
+        ftp( process.argv[2], process.argv[3], process.argv[4], function () {
+            console.log('PodSmash complete!');
+        });
+    }else{
+        console.log('Requires args: FTP Host, User, Password');
 }
 
+})();
